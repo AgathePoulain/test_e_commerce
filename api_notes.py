f27f412cc6b287c27518f34474cb2eeaf50d6dfb
chptte <agth.poulain@gmail.com>
2023-08-03 10:06:46 +0200

    Commit 6 bis

---
+++ api_notes.py
@@ -0,0 +1,51 @@
+"""
+            BUILD AN API WITH DJANGO REST FRAMEWORK
+
+0 - INTRODUCTION -
+Application Programming Interface : defines how different software systems should interact
+with each other by specifying the types of requests and responses that can be made as well as
+the format of data being transferred
+
+Web development context : API used to expose data and functionality from a web application
+to other systems (mobile apps, other websites, IOT devices)
+
+Example du restaurant : le client passe commande à la cuisine en passant par le serveur, qui
+traite la demande pur une solution appropriée
+
+API is capable of accepting and collecting different requests, and based of those requests,
+it returns different and appropriate responses
+
+
+1 - PROJECT SETUP -
+
+
+
+2 - CREATING FIRST ENDPOINT -
+3 - INSTALLING POSTMAN -
+4 - TEST API WITH REQUEST -
+5 - TEST API POSTMAN -
+6 - CREATING MODELS -
+7 - REGISTER MODELS IN ADMIN -
+8 - CREATING SUPERUSER -
+9 - CREATING POSTS FROM ADMIN -
+10 - GET ALL POST FROM ADMIN -
+11 - CREATING SERIALIZERS -
+12 - POST CREATE API ENDPOINT -
+13 - DELETE POST API ENDPOINT -
+14 - UPDATE POST API ENDPOINT -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
